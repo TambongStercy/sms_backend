5bf3cf3f96469832c95821105eb0a6a28c2ec78b
TambongStercy <stercytambong@gmail.com>
2025-03-28 09:43:29 +0100

    AI modifications

--- src/types/express.d.ts
+++ src/types/express.d.ts
@@ -1,9 +1,20 @@
 import { User } from '@prisma/client';
+import { Request } from 'express';
 
 declare global {
     namespace Express {
         interface Request {
             user?: User;
+            file?: Express.Multer.File;
+            files?: Express.Multer.File[];
         }
+    }
+}
+
+declare module 'express' {
+    interface Request {
+        user?: any;
+        file?: Express.Multer.File;
+        files?: Express.Multer.File[];
     }
 } 
